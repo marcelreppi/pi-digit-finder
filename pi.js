3b61907fdeb598ad56e4f0e258536abdd817fa1e
Marcel Reppenhagen <marcel.reppenhagen@campus.tu-berlin.de>
2017-10-22 11:32:09 +0200

    Added more urls for different amounts of pi digits

--- pi.js
+++ pi.js
@@ -1,7 +1,13 @@
 const https = require('https');
 
 const searchTerm = process.argv[2]
-const url = 'https://stuff.mit.edu/afs/sipb/contrib/pi/pi-billion.txt'
+
+// const url = 'https://www.angio.net/pi/digits/1000.txt'                   // thousand
+// const url = 'https://www.angio.net/pi/digits/10000.txt'                  // ten thousand
+const url = 'https://www.angio.net/pi/digits/100000.txt'                    // one hundred throusand
+// const url = 'https://www.angio.net/pi/digits/pi1000000.txt'              // one million
+// const url = 'https://stuff.mit.edu/afs/sipb/contrib/pi/pi-billion.txt'   // one billion
+
 let posBase = 0
 let lastChunk = ''
 let found = false
